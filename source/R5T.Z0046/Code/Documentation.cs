caa15eebdb2b0a4752746fe3897d1a88ceb9fbe4
davidcoats <david@getsafetycone.com>
2023-05-13 10:52:16 -0700

    Initial commit.

---
+++ source/R5T.Z0046/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0046
+{
+	/// <summary>
+	/// Strongly-typed example values for all purposes.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0046/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Z0046
+Strongly-typed example values for all purposes.
