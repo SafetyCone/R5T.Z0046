5d1446f79d86b0f2c1b8ca37d29fdac35df0436c
davidcoats <david@getsafetycone.com>
2023-06-14 00:33:48 -0700

    Interim changes.

---
+++ source/R5T.Z0046/Code/Values/IProjectFilePaths.cs
@@ -0,0 +1,28 @@
+using System;
+
+using R5T.T0131;
+using R5T.T0172;
+using R5T.T0172.Extensions;
+
+
+namespace R5T.Z0046
+{
+    [ValuesMarker]
+    public partial interface IProjectFilePaths : IValuesMarker
+    {
+        /// <summary>
+        /// <para>C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0060\source\R5T.S0060.S001\R5T.S0060.S001.csproj</para>
+        /// Contains a Program class, with Main() method, with a single statement calling into the R5T.F0000 library.
+        /// This is perfect for determining how to iterate over all statements in all types of a target project, and then accumulate the project locations of referenced types.
+        /// </summary>
+        public IProjectFilePath Example_SimpleWithProjectReference => @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0060\source\R5T.S0060.S001\R5T.S0060.S001.csproj".ToProjectFilePath();
+
+        /// <summary>
+        /// <para>C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0060\source\R5T.S0060.S002\R5T.S0060.S002.csproj</para>
+        /// Contains a Program class, with Main() method, with two statements:
+        ///     1. A simple statement calling into R5T.F0000 functionality.
+        ///     2. A statement calling into the CliWrap NuGet package.
+        /// </summary>
+        public IProjectFilePath Example_SimpleWithNuGetPackageReference => @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0060\source\R5T.S0060.S002\R5T.S0060.S002.csproj".ToProjectFilePath();
+    }
+}
---
+++ source/R5T.Z0046/Code/ΩInstances/ProjectFilePaths.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace R5T.Z0046
+{
+    public class ProjectFilePaths : IProjectFilePaths
+    {
+        #region Infrastructure
+
+        public static IProjectFilePaths Instance { get; } = new ProjectFilePaths();
+
+
+        private ProjectFilePaths()
+        {
+        }
+
+        #endregion
+    }
+}
